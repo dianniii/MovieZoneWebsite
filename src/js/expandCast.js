fe675c3e070fe81359722a05dc82044c1ccade32
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-21 23:15:09 +0200

    fix mistake in index.html, create movie.js to import all functions used in movie.html, add id to span element with hidden part of the cast, create js file and write function that show or hide hidden cast part, link file to movie.html

---
+++ src/js/expandCast.js
@@ -0,0 +1,15 @@
+const castFullMovieBtn = document.getElementById("castMovieBtn");
+const castPart = document.getElementById("castAdditionalPart");
+const castHiddenClassName = "full-movie__cast--hidden";
+
+function toggleCastElementLen() {
+  castPart.classList.toggle(castHiddenClassName);
+
+  if (castPart.classList.contains(castHiddenClassName)) {
+    castFullMovieBtn.textContent = "see more...";
+  } else {
+    castFullMovieBtn.textContent = "see less";
+  }
+}
+
+castFullMovieBtn.addEventListener("click", toggleCastElementLen);
