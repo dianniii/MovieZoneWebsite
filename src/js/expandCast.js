1317eadb621edc48b154afb83dc52946d82a3b29
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-21 23:26:56 +0200

    wrap expand cast logic with export function setUpMovieCastBtn, import setUpMovieCastBtn to movie.js, link movie.js as module to movie.html

--- src/js/expandCast.js
+++ src/js/expandCast.js
@@ -1,15 +1,17 @@
-const castFullMovieBtn = document.getElementById("castMovieBtn");
-const castPart = document.getElementById("castAdditionalPart");
-const castHiddenClassName = "full-movie__cast--hidden";
+export function setUpMovieCastBtn() {
+  const castFullMovieBtn = document.getElementById("castMovieBtn");
+  const castPart = document.getElementById("castAdditionalPart");
+  const castHiddenClassName = "full-movie__cast--hidden";
 
-function toggleCastElementLen() {
-  castPart.classList.toggle(castHiddenClassName);
+  function toggleCastElementLen() {
+    castPart.classList.toggle(castHiddenClassName);
 
-  if (castPart.classList.contains(castHiddenClassName)) {
-    castFullMovieBtn.textContent = "see more...";
-  } else {
-    castFullMovieBtn.textContent = "see less";
+    if (castPart.classList.contains(castHiddenClassName)) {
+      castFullMovieBtn.textContent = "see more...";
+    } else {
+      castFullMovieBtn.textContent = "see less";
+    }
   }
-}
 
-castFullMovieBtn.addEventListener("click", toggleCastElementLen);
+  castFullMovieBtn.addEventListener("click", toggleCastElementLen);
+}
---
+++ src/movie.js
@@ -0,0 +1,3 @@
+import { setUpMovieCastBtn } from "./js/expandCast.js"; //Import setUpMovieCastBtn function from expandCast.js
+
+setUpMovieCastBtn(); //call imported setUpMovieCastBtn function. It sets up the logic for the "see more" button (id=castMovieBtn), including binding the event handler.
