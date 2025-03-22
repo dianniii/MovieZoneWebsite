c95fb6aa5023ac0ec49a4a7300cb0de26bae6cea
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-22 02:26:58 +0200

    write function for filtering movie data

--- src/js/fullMovieDescription.js
+++ src/js/fullMovieDescription.js
@@ -1,8 +1,52 @@
 const movieContainer = document.getElementById("movie");
 let movie_id = "27205";
+// let movie_id = "1";
 const domenPartUrl = "https://movies.gila.workers.dev";
 const pathForFullMovieDescription = "/search/movie/description?";
 const searchParameters = `movie_id=${movie_id}`;
+
+const likeIconPath = "./public/icons/likeIcon.svg";
+const addMovieIconPath = "./public/icons/plusIcon.svg";
+const TMBDIconPath = "./public/icons/TMBD.svg";
+const filmIconPath = "./public/icons/filmIcon.svg";
+
+const classNames = {
+  banner: "full-movie__banner",
+  title: "full-movie__title",
+  tagline: ["full-movie__tagline", "full-movie__feature"],
+  controlBar: "full-movie__control-bar",
+};
+
+const propertyNames = [
+  "id",
+  "title",
+  "tagline",
+  "backdrop_path",
+  "genres", // (array)
+  "homepage",
+  "origin_country", // (array)
+  "spoken_languages", //(array of objects, where we need english_name)
+  "overview",
+  "release_date",
+  "vote_average",
+  "cast", // (array)
+  "directors", // (array)
+];
+
+// function createElementWithClass(tag, className) {
+//   const element = document.createElement(tag);
+
+//   if (Array.isArray(className)) {
+//     element.classList.add(className);
+//   } else {
+//     className.forEach((name) => element.classList.add(name));
+//   }
+//   return element;
+// }
+
+// function createMovieBanner(movieData) {
+//   const banner = createElementWithClass("div", classNames.banner);
+// }
 
 async function fetchMovieObj() {
   try {
@@ -11,20 +55,32 @@ async function fetchMovieObj() {
     );
 
     if (!response.ok) {
-      movieContainer.innerHTML =
-        "Не удалось загрузить данные фильма. Попробуйте позже.";
-      return;
+      throw new Error("Не удалось загрузить данные фильма");
     }
 
     const movieData = await response.json();
-    console.log(movieData);
-    console.log(typeof movieData);
+    // console.log(movieData);
     return movieData;
   } catch (error) {
     console.error("Ошибка:", error);
     movieContainer.innerHTML =
       "Произошла ошибка при загрузке данных. Попробуйте позже.";
+    return null;
   }
 }
 
-fetchMovieObj();
+async function filterMovieData() {
+  const movieData = await fetchMovieObj();
+
+  if (!movieData) {
+    return;
+  }
+
+  const movieDescriptionObj = Object.fromEntries(
+    Object.entries(movieData).filter((arr) => propertyNames.includes(arr[0]))
+  );
+  console.log(movieDescriptionObj);
+  return movieDescriptionObj;
+}
+
+filterMovieData();
