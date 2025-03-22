6472e5692e2663ceb3f66245c74f1783444674a9
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-23 01:33:52 +0200

    fix class name for hidden part of cast in expandCast.js, add pic for default movie banner BG, change movie banner BG picture to default BG in scss file

--- src/js/expandCast.js
+++ src/js/expandCast.js
@@ -1,7 +1,7 @@
 export function setUpMovieCastBtn() {
   const castFullMovieBtn = document.getElementById("castMovieBtn");
   const castPart = document.getElementById("castAdditionalPart");
-  const castHiddenClassName = "full-movie__value--hidden";
+  const castHiddenClassName = "full-movie__feature-value--hidden";
 
   function toggleCastElementLen() {
     castPart.classList.toggle(castHiddenClassName);
