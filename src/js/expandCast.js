d5e1f993f5bdc0079827615698a2461ed9faf74e
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-22 02:36:05 +0200

    delete unused classes from html, change classname for hidden part of cast (full-movie__cast--hidden to full-movie__value--hidden)

--- src/js/expandCast.js
+++ src/js/expandCast.js
@@ -1,7 +1,7 @@
 export function setUpMovieCastBtn() {
   const castFullMovieBtn = document.getElementById("castMovieBtn");
   const castPart = document.getElementById("castAdditionalPart");
-  const castHiddenClassName = "full-movie__cast--hidden";
+  const castHiddenClassName = "full-movie__value--hidden";
 
   function toggleCastElementLen() {
     castPart.classList.toggle(castHiddenClassName);
