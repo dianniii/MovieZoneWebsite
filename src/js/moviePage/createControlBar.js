889cf4618f5c6ce28275332cc7149febcd37ebc5
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-25 19:04:33 +0200

    import iconPaths obj and isMovieStored funcrion to controlBar.js.

--- src/js/moviePage/createControlBar.js
+++ src/js/moviePage/createControlBar.js
@@ -1,34 +1,40 @@
-import {
-  likeIconPath,
-  addIconPath,
-  classesControlBar,
-  controlBarIds,
-} from "./movieVars";
-import { createElementWithProps, createButtonWithIcon } from "./movieUtils";
+import { iconPaths, classesControlBar, controlBarIds } from "./movieVars";
 
-export function createControlBarElem() {
+import {
+  createElementWithProps,
+  createButtonWithIcon,
+  isMovieStored,
+} from "./movieUtils";
+
+export function createControlBarElem(movieData) {
   const controlBarElem = createElementWithProps(
     "div",
     classesControlBar.controlBar
   );
+  //проверить есть ли в localStorage в watched этот фильм (по movie_id)
+  const watched = isMovieStored(movieData.id, "watched");
 
-  const likeBtnElem = createButtonWithIcon(
+  // const watched = isMovieWatched
+  const watchedBtnElem = createButtonWithIcon(
     controlBarIds.likeBtnId,
     classesControlBar.button,
     classesControlBar.icon,
-    likeIconPath,
-    "like icon"
+    watched ? iconPaths.dislikeIcon : iconPaths.likeIcon,
+    watched ? "dislike icon" : "like icon"
   );
 
-  const addBtnElem = createButtonWithIcon(
+  //проверить есть ли в localStorage в watchlist этот фильм (по movie_id)
+  const inWatchList = isMovieStored(movieData.id, "watchlist");
+
+  const watchListBtnElem = createButtonWithIcon(
     controlBarIds.addBtnId,
     classesControlBar.button,
     classesControlBar.icon,
-    addIconPath,
-    "plus icon"
+    inWatchList ? iconPaths.removeIcon : iconPaths.addIcon,
+    inWatchList ? "tick icon" : "plus icon"
   );
 
-  controlBarElem.append(likeBtnElem, addBtnElem);
+  controlBarElem.append(watchedBtnElem, watchListBtnElem);
 
   return controlBarElem;
 }
