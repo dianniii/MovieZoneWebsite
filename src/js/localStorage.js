0e1d68569301a4c499d13f7657592dff27233050
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-26 06:34:04 +0200

    fix bugs in local storage functions

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,24 +1,27 @@
 export function isMovieStored(movie_id, property) {
-  const moviesStored = JSON.parse(window.localStorage.getItem(property)) || [];
+  if (!window.localStorage.getItem(property)) {
+    window.localStorage.setItem(property, JSON.stringify([]));
+    return false;
+  }
+  const moviesStored = JSON.parse(window.localStorage.getItem(property));
   return moviesStored.some((movie) => Number(movie.id) === movie_id);
 }
 
 export function deleteMovieFromStorage(movie_id, property) {
-  const moviesStored = JSON.parse(window.localStorage.getItem(property)) || [];
   if (!isMovieStored(movie_id, property)) {
     return;
   }
+  const moviesStored = JSON.parse(window.localStorage.getItem(property));
   const newMovies = moviesStored.filter(
     (movie) => Number(movie.id) !== movie_id
   );
-  window.localStorage.setItem(property, newMovies);
+  window.localStorage.setItem(property, JSON.stringify(newMovies));
 }
 
 export function addMovieToStorage(movieObj, property) {
-  if (isMovieStored(movieObj.id, property)) {
-    return;
+  const moviesStored = JSON.parse(window.localStorage.getItem(property));
+  if (!isMovieStored(movieObj.id, property)) {
+    moviesStored.push(movieObj);
+    window.localStorage.setItem(property, JSON.stringify(moviesStored));
   }
-  const moviesStored = JSON.parse(window.localStorage.getItem(property)) || [];
-  moviesStored.push(movieObj);
-  window.localStorage.setItem(property, JSON.stringify(moviesStored));
 }
