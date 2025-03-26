4fff9c49e357d4737890a4ebb551fb8218593362
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-26 03:56:03 +0200

    write functions that add and delete info about movie from the local storage

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,4 +1,24 @@
-export function isMovieStored(movie_id, propName) {
-  const moviesStored = JSON.parse(window.localStorage.getItem(propName)) || [];
-  return moviesStored.some((movie) => +movie.id === movie_id);
+export function isMovieStored(movie_id, property) {
+  const moviesStored = JSON.parse(window.localStorage.getItem(property)) || [];
+  return moviesStored.some((movie) => Number(movie.id) === movie_id);
+}
+
+export function deleteMovieFromStorage(movie_id, property) {
+  const moviesStored = JSON.parse(window.localStorage.getItem(property)) || [];
+  if (!isMovieStored(movie_id, property)) {
+    return;
+  }
+  const newMovies = moviesStored.filter(
+    (movie) => Number(movie.id) !== movie_id
+  );
+  window.localStorage.setItem(property, newMovies);
+}
+
+export function addMovieToStorage(movieObj, property) {
+  if (isMovieStored(movieObj.id, property)) {
+    return;
+  }
+  const moviesStored = JSON.parse(window.localStorage.getItem(property)) || [];
+  moviesStored.push(movieObj);
+  window.localStorage.setItem(property, JSON.stringify(moviesStored));
 }
