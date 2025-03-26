d1850c68f6fcab324e7915a9d6cdab1bdb8dd6c5
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-26 03:40:51 +0200

    rename movieData.js to getMovieData, add function for getting movie_id from url and save short info about movie

--- src/js/moviePage/getMovieData.js
+++ src/js/moviePage/getMovieData.js
@@ -1,7 +1,8 @@
 import {
   domenPartUrl,
   pathForFullMovieDescription,
-  propertyNames,
+  fullPropertylist,
+  shortPropertyList,
 } from "./movieVars";
 
 export async function fetchMovieObj(movie_id) {
@@ -37,8 +38,26 @@ export function filterMovieData(movieData) {
   }
 
   const movieDescriptionObj = Object.fromEntries(
-    Object.entries(movieData).filter((arr) => propertyNames.includes(arr[0]))
+    Object.entries(movieData).filter(([key, value]) =>
+      fullPropertylist.includes(key)
+    )
   );
 
   return movieDescriptionObj;
+}
+
+export function getMovieIdFromURL() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("id");
+}
+
+export function saveShortMovieData(movieData) {
+  const shortMovieData = {};
+  const movieKeyValArr = Object.entries(movieData);
+  for (const [key, value] of movieKeyValArr) {
+    if (shortPropertyList.includes(key)) {
+      shortMovieData[key] = value;
+    }
+  }
+  return shortMovieData;
 }
