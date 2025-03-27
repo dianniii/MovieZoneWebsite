47b68a1968f23e603732fa7dd3889118c5a7f435
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-27 14:57:42 +0200

    create obj with class names

--- src/js/savedMovies/savedMoviesVars.js
+++ src/js/savedMovies/savedMoviesVars.js
@@ -1,5 +1,12 @@
-// const movieContainerClass = "movie";
-// const movieSaveClasses = {
-//     movie: "movie";
-
-// }
+const movieContainerClass = "movie__";
+const savedMoviesClss = {
+  movieContainer: movieContainerClass,
+  title: `${movieContainerClass}title`,
+  imgContainer: `${movieContainerClass}img-container`,
+  img: `${movieContainerClass}img`,
+  year: `${movieContainerClass}year`,
+  overview: `${movieContainerClass}description`,
+  linkContainer: `${movieContainerClass}link-container`,
+  link: `${movieContainerClass}link`,
+  linkIcon: `${movieContainerClass}link-icon`,
+};
