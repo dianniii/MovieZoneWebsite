0d35e12efb235968394d738e64fdadc33632a9c7
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-03-31 15:41:24 +0300

    make separate function for moving to other page, make movieCardClickHandler suitable to use in movie banner (random movie and movie page)

--- src/js/movieCardClickHandler.js
+++ src/js/movieCardClickHandler.js
@@ -4,16 +4,23 @@ import {
 } from "./savedMovies/savedMoviesVars";
 import { handleRemoveMovieClick } from "./savedMovies/removeBtnHandler";
 import { handleControlBarClick } from "./controlBar/controlBarBtnHandlers";
+import { classesBanner } from "./moviePage/movieVars";
+import { classesControlBar } from "../js/controlBar/controlBarVars";
 
 export function movieCardClickHandler(evt) {
   if (evt.target.closest("." + classesControlRemBar.controlBarElem[1])) {
-    console.log("close");
     handleRemoveMovieClick(evt);
-  } else if (evt.target.closest("." + classesControlRemBar.controlBarElem[0])) {
+  } else if (evt.target.closest("." + classesControlBar.controlBar)) {
     handleControlBarClick(evt);
   } else if (evt.target.closest("." + savedMoviesClss.movieCard)) {
-    const movieElem = evt.target.closest(".movie-card");
-    const movie_id = movieElem.dataset.id;
-    window.location.href = `movie.html?id=${movie_id}`;
+    moveToMoviePage(evt.target.closest(".movie-card"), "movie");
+  } else if (evt.target.closest("." + classesBanner.banner)) {
+    moveToMoviePage(evt.target.closest("." + classesBanner.banner), "movie");
   }
+}
+
+export function moveToMoviePage(elem, pageName) {
+  console.log(elem);
+  const id = elem.dataset.id;
+  window.location.href = `${pageName}.html?id=${id}`;
 }
